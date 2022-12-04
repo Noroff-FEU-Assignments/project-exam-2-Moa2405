f9b496a4201028b944f860956c7e7b4297d0463c
Moa2405 <62264443+Moa2405@users.noreply.github.com>
2022-12-04 23:50:56 +0100

    fixed edit post modal

--- src/components/post/editPost/EditPost.jsx
+++ src/components/post/editPost/EditPost.jsx
@@ -119,11 +119,12 @@ const EditPost = ({ post, closeMenu }) => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
     borderRadius: "10px",
     border: "2px solid",
     borderColor: theme.palette.mode === "dark" ? theme.palette.grey[800] : theme.palette.grey[500],
     padding: "1rem",
+    width: "100%",
+    maxWidth: "500px",
   };
 
   return (
