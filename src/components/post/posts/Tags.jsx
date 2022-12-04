d1fc67c7c2d5d36d820f2a299ebfecc8a89504c3
Moa2405 <62264443+Moa2405@users.noreply.github.com>
2022-12-04 20:58:51 +0100

    changed tag color on light theme

--- src/components/post/posts/Tags.jsx
+++ src/components/post/posts/Tags.jsx
@@ -1,14 +1,26 @@
 import { Stack, Typography } from "@mui/material";
 import PropTypes from "prop-types";
+import { useTheme } from "@mui/system";
 
 const Tags = ({ tags }) => {
 
+  const theme = useTheme()
   const filteredTags = tags.filter((tag) => tag !== "");
+
+  const tagsWithHash = filteredTags.map((tag) => {
+    if (tag.charAt(0) !== "#") {
+      return "#" + tag;
+    }
+    return tag;
+  });
+
+
+  const color = theme.palette.mode === "dark" ? "primary" : "contrastText";
 
   return (
     <Stack spacing={1} direction="row">
-      {filteredTags.map((tag, index) => (
-        <Typography key={index} color="primary" variant="body2">
+      {tagsWithHash.map((tag, index) => (
+        <Typography key={index} color={color} variant="body2">
           {tag}
         </Typography>
       ))}
